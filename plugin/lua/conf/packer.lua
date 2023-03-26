cebcaf459e3483d90073817ab8d2d51fe0f927a9
ericdbishop <ericdbishop@gmail.com>
2023-03-26 10:08:50 -0400

    Adding current nvim configuration

---
+++ plugin/lua/conf/packer.lua
@@ -0,0 +1,56 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use ({
+	  'tanvirtin/monokai.nvim',
+	  as = 'monokai',
+	  config = function()
+		  require('monokai').setup {}
+		  require('monokai').setup { palette = require('monokai').pro }
+	  end
+  })
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use("nvim-treesitter/playground")
+  use("theprimeagen/harpoon")
+  use("mbbill/undotree")
+  use("tpope/vim-fugitive")
+  use("nvim-treesitter/nvim-treesitter-context");
+  use("lervag/vimtex")
+
+  use {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v1.x',
+          requires = {
+                  -- LSP Support
+                  {'neovim/nvim-lspconfig'},
+                  {'williamboman/mason.nvim'},
+                  {'williamboman/mason-lspconfig.nvim'},
+
+                  -- Autocompletion
+                  {'hrsh7th/nvim-cmp'},
+                  {'hrsh7th/cmp-buffer'},
+                  {'hrsh7th/cmp-path'},
+                  {'saadparwaiz1/cmp_luasnip'},
+                  {'hrsh7th/cmp-nvim-lsp'},
+                  {'hrsh7th/cmp-nvim-lua'},
+
+                  -- Snippets
+                  {'L3MON4D3/LuaSnip'},
+                  {'rafamadriz/friendly-snippets'},
+          }
+  }
+
+end)
